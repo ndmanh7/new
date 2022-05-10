4dc7ee7a36f53cc01478c57d1081c940fbe21f36
manh <ndmanh10a7@gmail.com>
2022-05-10 20:12:11 +0700

    Update Ham.java

--- src/Ham.java
+++ src/Ham.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Ham {
 	//thu them vao xem
-	//them vao lan 2
+	//them vao lan 2 nay
 	 static void inThongTin(String name) {					//them static vao de goi duoc trong ham main
 															//vi ham main la static va ham main chi goi duoc ham static
 		System.out.println("Ten : "+name);
